84a13537c7e4ecf5e5210671b88d2dfeae013ab3
SHANDZIN <miqueiasantos200@gmail.com>
2026-09-15 13:01:01 -0300

    deen: Sync cmdline with CAF
    Signed-off-by: SHANDZIN <miqueiasantos200@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,8 +33,8 @@ TARGET_NO_BOOTLOADER := true
 TARGET_SCREEN_DENSITY := 320
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=400M androidboot.hab.product=deen androidboot.usbconfigfs=true loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive androidboot.fastboot=1
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci androidboot.boot_devices=soc/7824900.sdhci earlycon=msm_serial_dm,0x78af000 androidboot.usbconfigfs=true loop.max_part=7 androidboot.hab.product=deen
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 
 BOARD_KERNEL_BASE := 0x80000000
